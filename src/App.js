f96973254bade95c3113ed983a767bd80d0bc808
vikas kumar <vk.vikas3003@gmail.com>
2023-02-24 19:27:17 +0530

    search feature added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-
+import { useState } from 'react';
 import './App.css';
 import { users } from './users';
 
 function App() {
+  const [query,setQuery] = useState(users);
+
   return (
     <div className="App">
-      <input type='text' placeholder='search' />
+      <input type='text' placeholder='search' onChange={e => setQuery(e.target.value)}/>
       <ul>
-        {users.map(i => (
-          <li key={i.id}>{i.first_name}</li>
+        {users.filter(i => i.first_name.toLocaleLowerCase().includes(query)).map(i => (
+          <li key={i.id}>
+          {i.first_name}</li>
         ))}
       </ul>
     </div>
